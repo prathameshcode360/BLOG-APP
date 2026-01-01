d25edf7023a5c8f5dde9b2c7ac4139f9cc42cd79
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2026-01-01 12:55:37 +0530

    Passing Object in UseState

--- src/Component/blog.js
+++ src/Component/blog.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 function Blog() {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [content, setContent] = useState("");
+  const [formData, setFormData] = useState({ title: "", content: "" });
   const [blogs, setBlog] = useState([]);
   function handleSubmit(event) {
     event.preventDefault();
-    setBlog([{ title, content }, ...blogs]);
-    setTitle("");
-    setContent("");
+    setBlog([{ title: formData.title, content: formData.content }, ...blogs]);
+    setFormData({ title: "", content: "" });
   }
   return (
     <div className="main-container">
@@ -19,15 +19,19 @@ function Blog() {
           <input
             type="text"
             placeholder="Enter Title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            value={formData.title}
+            onChange={(e) =>
+              setFormData({ title: e.target.value, content: formData.content })
+            }
           />
           <label>Content</label>
           <input
             type="text"
             placeholder="Enter Title"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            value={formData.content}
+            onChange={(e) =>
+              setFormData({ title: formData.title, content: e.target.value })
+            }
           />
           <button type="submit">Add</button>
         </form>
